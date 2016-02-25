c6e8b7a76eb64d29df56dd479ff1a67cb0886491
Thiago <thiago.miranda@sambatech.com.br>
2016-02-25 18:09:54 -0300

    teste do ios

--- TesteMobileIOS/ViewController.swift
+++ TesteMobileIOS/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import MobilePlayer
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let videoURL = NSURL(string:  "http://gbbrfd.sambavideos.sambatech.com/account/37/2/2015-11-05/video/cb7a5d7441741d8bcb29abc6521d9a85/marina_360p.mp4")!
+
+        let playerVC = MobilePlayerViewController(contentURL: videoURL)
+        
+        playerVC.title = "Teste Mobile"
+        playerVC.activityItems = [videoURL]
+        presentMoviePlayerViewControllerAnimated(playerVC)
+
+        
+        self.playerContainer.addSubview(playerVC.view)
+        
     }
 
     override func didReceiveMemoryWarning() {
