339ad6358257ba56721769480ff0d20a07e4a074
Leandro Zanol <lzanol@gmail.com>
2016-03-11 18:52:45 -0300

    #3 WIP: live streams

--- TesteMobileIOS/SambaMediaRequest.swift
+++ TesteMobileIOS/SambaMediaRequest.swift
@@ -12,9 +12,29 @@ public class SambaMediaRequest {
 	
 	public var projectHash: String
 	public var mediaId: String?
+	public var streamName: String?
+	public var streamUrls: [String]?
 	
-	init(projectHash: String, mediaId: String?) {
+	init(projectHash: String, mediaId: String?, streamName: String?, streamUrls: [String]?) {
 		self.projectHash = projectHash
 		self.mediaId = mediaId
+		self.streamName = streamName
+		self.streamUrls = streamUrls
+	}
+	
+	public convenience init(projectHash: String, streamUrls: [String]) {
+		self.init(projectHash: projectHash, mediaId: nil, streamName: nil, streamUrls: streamUrls)
+	}
+	
+	public convenience init(projectHash: String, streamUrl: String) {
+		self.init(projectHash: projectHash, mediaId: nil, streamName: nil, streamUrls: [streamUrl])
+	}
+	
+	public convenience init(projectHash: String, streamName: String) {
+		self.init(projectHash: projectHash, mediaId: nil, streamName: streamName, streamUrls: nil)
+	}
+	
+	public convenience init(projectHash: String, mediaId: String) {
+		self.init(projectHash: projectHash, mediaId: mediaId, streamName: nil, streamUrls: nil)
 	}
 }
