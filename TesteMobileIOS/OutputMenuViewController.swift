1d09e519691f68f4f57fd68e0b3867be361552fd
Leandro Zanol <lzanol@gmail.com>
2016-03-29 19:33:08 -0300

    #7 WIP: output menu (3)

--- TesteMobileIOS/OutputMenuViewController.swift
+++ TesteMobileIOS/OutputMenuViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 
 class OutputMenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIViewControllerTransitioningDelegate {
 	
+	@IBOutlet var tableView: UITableView!
+	@IBOutlet var cancelButton: UIButton!
+	
 	private let cellIdentifier: String = "outputCell"
 	private let player: SambaPlayer
 	private let outputs: [SambaMedia.Output]
@@ -24,7 +27,9 @@ class OutputMenuViewController: UIViewController, UITableViewDataSource, UITable
 		transitioningDelegate = self
 		modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
 		
-		createContent()
+		view = NSBundle.mainBundle().loadNibNamed("OutputMenu", owner: self, options: nil).first as! UIView
+		
+		cancelButton.addCallback({ self.close() }, forControlEvents: .TouchUpInside)
 	}
 
 	required init?(coder aDecoder: NSCoder) {
@@ -56,50 +61,5 @@ class OutputMenuViewController: UIViewController, UITableViewDataSource, UITable
 	func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
 		print(outputs[indexPath.row])
 		close()
-	}
-	
-	private func createContent() {
-		let margin = CGFloat(16)
-		var contentSize = CGSizeMake(view.frame.width - 60, 0)
-		
-		let tableView = UITableView(frame: CGRectMake(0, 0, contentSize.width - margin, contentSize.height), style: UITableViewStyle.Plain)
-		tableView.dataSource = self
-		tableView.delegate = self
-		tableView.layoutIfNeeded()
-		var tableFrame = tableView.frame
-		tableFrame.size.height = tableView.contentSize.height
-		tableView.frame = tableFrame
-		tableView.center = view.center
-
-		let closeButton = UIButton(type: UIButtonType.System)
-		closeButton.setTitle("Cancelar", forState: UIControlState.Normal)
-		closeButton.sizeToFit()
-		var btFrame = closeButton.frame
-		btFrame.origin = CGPointMake(tableView.frame.origin.x + tableView.frame.width - btFrame.width,
-			tableView.frame.origin.y + tableView.frame.height + margin/2)
-		closeButton.frame = btFrame
-		
-		closeButton.addCallback({ self.close() }, forControlEvents: .TouchUpInside)
-		
-		/*let label = UIButton()
-		label.setTitle("Qualidade", forState: UIControlState.Normal)
-		label.sizeToFit()
-		var labelFrame = closeButton.frame
-		labelFrame.origin = CGPointMake(tableView.frame.origin.x, tableView.frame.origin.y)
-		label.frame = labelFrame*/
-		
-		contentSize.height = tableView.contentSize.height + margin + closeButton.frame.height*2
-		
-		let bgView = UIView(frame: CGRect(origin: view.center, size: contentSize))
-		bgView.center = view.center
-		bgView.backgroundColor = UIColor.whiteColor()
-		bgView.layer.cornerRadius = 5
-		
-		view.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5)
-		
-		view.addSubview(bgView)
-		//view.addSubview(label)
-		view.addSubview(tableView)
-		view.addSubview(closeButton)
 	}
 }
