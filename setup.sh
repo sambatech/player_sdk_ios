d74f2941ad3cf1775d688cc53ba822becf46515d
Leandro Zanol <lzanol@gmail.com>
2016-07-01 13:33:17 -0300

    #8 fixed build script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
+#!/bin/sh
+
+if [[ ! -n "$EXECUTABLE_PATH" ]]; then
+	SRCROOT=$(dirname "$0")
+	WRAPPER_NAME="SambaPlayer.framework"
+	EXECUTABLE_PATH="${WRAPPER_NAME}/SambaPlayer"
+fi
+
 # trusting on Carthage dir structs
-if [ ! -d "${SRCROOT}/../../../Carthage" ]; then
+if [[ ! -d "${SRCROOT}/../../../Carthage/Build" ]]; then
     echo 'Warning: No Carthage build found, merges will not happen.'
     exit
 fi
@@ -9,4 +17,5 @@ cp -r "${SRCROOT}/Build/Products/Release-iphonesimulator/${WRAPPER_NAME}/Modules
 lipo -create "${SRCROOT}/Build/Products/Release-iphonesimulator/${EXECUTABLE_PATH}" "${SRCROOT}/../../Build/iOS/${EXECUTABLE_PATH}" -output "${SRCROOT}/../../Build/iOS/${EXECUTABLE_PATH}"
 
 # copies IMA framework to Carthage's build dir
+#echo "copying IMA: ${SRCROOT}/Frameworks/GoogleInteractiveMediaAds.framework => ${SRCROOT}/../../Build/iOS"
 cp -r "${SRCROOT}/Frameworks/GoogleInteractiveMediaAds.framework" "${SRCROOT}/../../Build/iOS"
