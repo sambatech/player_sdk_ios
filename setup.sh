8da1e02696ec38dd86ea10ff2f8824daf3746c7a
Leandro Zanol <lzanol@gmail.com>
2016-07-01 11:18:01 -0300

    #8 some build automation

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# trusting on Carthage dir structs
+if [ ! -d "${SRCROOT}/../../Carthage" ]; then
+    echo 'Warning: No Carthage build found, merges will not happen.'
+    exit
+fi
+
+# merges lib to reach all archs
+cp -r "${SRCROOT}/Build/Products/Release-iphonesimulator/${WRAPPER_NAME}/Modules" "${SRCROOT}/../../Build/iOS/${WRAPPER_NAME}/"
+lipo -create "${SRCROOT}/Build/Products/Release-iphonesimulator/${EXECUTABLE_PATH}" "${SRCROOT}/../../Build/iOS/${EXECUTABLE_PATH}" -output "${SRCROOT}/../../Build/iOS/${EXECUTABLE_PATH}"
+
+# copies IMA framework to Carthage's build dir
+cp -r "$(SRCROOT)/Frameworks/GoogleInteractiveMediaAds.framework" "${SRCROOT}/../../Build/iOS"
