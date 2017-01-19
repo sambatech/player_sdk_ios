98b8bc5b205ef6226a4819936b0a24d697600704
Leandro Zanol <lzanol@gmail.com>
2017-01-19 20:25:52 -0200

    #525 WIP: setting caption read...

--- SambaPlayer/CaptionsScreen.swift
+++ SambaPlayer/CaptionsScreen.swift
@@ -15,11 +15,18 @@ class CaptionsScreen : UIViewController, SambaPlayerDelegate {
 	@IBOutlet var textField: UILabel!
 	
 	private let _player: SambaPlayer
-	private let _captions: [SambaMediaCaption]
+	private let _captionConfigs: [SambaMediaCaption]
+	private var _captions = [Caption]()
+	private var _captionsMap = [Int:[Caption]]()
+	private var _parsed = false
+	
+	private struct Caption {
+		let index: Int, startTime: Float, endTime: Float, text: String
+	}
 	
 	init(player: SambaPlayer, captions: [SambaMediaCaption]) {
 		_player = player
-		_captions = captions
+		_captionConfigs = captions
 		
 		super.init(nibName: "CaptionsScreen", bundle: Bundle(for: type(of: self)))
 		
@@ -32,19 +39,19 @@ class CaptionsScreen : UIViewController, SambaPlayerDelegate {
 	}
 	
 	override func viewWillAppear(_ animated: Bool) {
-		if let index = _captions.index(where: { $0.isDefault }) {
+		if let index = _captionConfigs.index(where: { $0.isDefault }) {
 			changeCaption(index)
 		}
 	}
 	
 	func changeCaption(_ value: Int) {
 		guard value != currentIndex,
-			value < _captions.count else { return }
+			value < _captionConfigs.count else { return }
 		
 		currentIndex = value
 		
 		// disable
-		if _captions[value].url == "" {
+		if _captionConfigs[value].url == "" {
 			textField.text = ""
 			view.isHidden = true
 			return
@@ -52,50 +59,12 @@ class CaptionsScreen : UIViewController, SambaPlayerDelegate {
 		
 		view.isHidden = false
 		
-		Helpers.requestURL(_captions[value].url, { (response: String?) in
+		Helpers.requestURL(_captionConfigs[value].url, { (response: String?) in
 			guard let response = response else { return }
+			print("parsing...")
 			self.parse(response)
 		}) { (error, response) in
 			print(error, response)
-		}
-	}
-	
-	func parse(cations: String) {
-		var captions = [Caption]()
-		var index = -1
-		var startTime: Float = 0.0
-		var endTime: Float = 0.0
-		var text = ""
-		var count = 0
-		var time = [String]()
-		var offset = 0
-		
-		for match in Helpers.matchesForRegexInText(".+(!?[\\r\\n])", text: captions) {
-			// if caption index occurence
-			if let r = match.range(of: "^\\d+(?=[\\r\\n]$)", options: .regularExpression) {
-				captions.append(Caption(index: index, startTime: startTime, endTime: endTime, text: text))
-				index = Int(match[r]) ?? -1
-				startTime = 0.0
-				endTime = 0.0
-				text = ""
-				count = 1
-				print(index)
-				continue
-			}
-			
-			switch count {
-			case 1:
-				time = matchesForRegexInText("\\d+", text: match)
-				startTime = extractTime(time)
-				endTime = extractTime(time, offset: 4)
-				
-				print(startTime, endTime)
-			default:
-				text += match.replacingOccurrences(of: "[\\r\\n]", with: " ", options: .regularExpression)
-				print(text)
-			}
-			
-			count += 1
 		}
 	}
 	
@@ -107,14 +76,83 @@ class CaptionsScreen : UIViewController, SambaPlayerDelegate {
 	func onPause() {}
 	
 	func onProgress() {
+		guard _parsed else { return }
 		
+		let time = _player.currentTime
+		
+		if _captionsMap.index(forKey: Int(time/60)) {
+			
+		}
+		
+		for caption in captions where caption.startTime >= time && caption.endTime <= time {
+			
+		}
 	}
 	
 	func onFinish() {}
 	func onDestroy() {}
 	func onError(_ error: SambaPlayerError) {}
 	
-	func extractTime(_ time: [String], offset: Int = 0) -> Float {
+	private func parse(_ captionsText: String) {
+		_parsed = false
+		
+		var index = -1
+		var startTime: Float = 0.0
+		var endTime: Float = 0.0
+		var text = ""
+		var count = 0
+		var time = [String]()
+		var offset = 0
+		var m = 0
+		var mLast = -1
+		
+		_captions = [Caption]()
+		
+		for match in Helpers.matchesForRegexInText(".+(!?[\\r\\n])", text: captionsText) {
+			// caption index occurence
+			if let r = match.range(of: "^\\d+(?=[\\r\\n]$)", options: .regularExpression) {
+				// ignore first time
+				if index != -1 {
+					_captions.append(Caption(index: index, startTime: startTime, endTime: endTime, text: text))
+					m = Int(startTime/60)
+					
+					if mLast == -1 {
+						mLast = m
+					}
+					else if m != mLast {
+						mLast = m
+						_captionsMap[m] = _captions
+						_captions = [Caption]()
+					}
+				}
+				
+				index = Int(match[r]) ?? -1
+				startTime = 0.0
+				endTime = 0.0
+				text = ""
+				count = 1
+				print(index)
+				continue
+			}
+			
+			switch count {
+			case 1:
+				time = Helpers.matchesForRegexInText("\\d+", text: match)
+				startTime = extractTime(time)
+				endTime = extractTime(time, offset: 4)
+				print(startTime, endTime)
+			default:
+				text += match.replacingOccurrences(of: "[\\r\\n]", with: " ", options: .regularExpression)
+				print(text)
+			}
+			
+			count += 1
+		}
+		
+		_parsed = true
+	}
+	
+	private func extractTime(_ time: [String], offset: Int = 0) -> Float {
 		guard time.count > 0 && (time.count + offset)%4 == 0 else { return 0.0 }
 		
 		let h = (Int(time[offset + 0]) ?? 0)*3600
@@ -124,8 +162,8 @@ class CaptionsScreen : UIViewController, SambaPlayerDelegate {
 		
 		return Float(h + m + s) + Float(ms)/1000.0
 	}
-}
-
-struct Caption {
-	let index: Int, startTime: Float, endTime: Float, text: String
+	
+	private func retriveCaption(time: Int) {
+		return _captions[0]
+	}
 }
