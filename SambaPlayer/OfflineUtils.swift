315ac773499b42927531230966114f694f667640
kesleyvaz <kesley.vaz@sambatech.com.br>
2018-11-29 14:54:49 -0200

    media offline model 2

--- SambaPlayer/OfflineUtils.swift
+++ SambaPlayer/OfflineUtils.swift
@@ -9,12 +9,37 @@
 import Foundation
 
 
-class OfflineUtils {
+public class OfflineUtils {
+    
+    private static let MEDIAS_KEY = "MEDIAS_KEY"
     
     private init(){}
     
-    static func persistMedia(_ medias: [SambaMediaConfig]) {
+    public static func persistMedias(_ medias: [SambaMediaConfig]) {
         
+        let mediaOffline = medias.map{SambaOfflineMedia.from(sambaMedia: $0)}
+        
+        let jsonData = try? JSONEncoder().encode(mediaOffline)
+        
+        guard let data = jsonData else {return}
+        
+        UserDefaults.standard.set(data, forKey: MEDIAS_KEY)
+    }
+    
+    public static func getPersistedMedias() -> [SambaMediaConfig]? {
+        let jsonData = UserDefaults.standard.data(forKey: MEDIAS_KEY)
+        
+        guard let data = jsonData else {
+            return nil
+        }
+    
+        let mediasOffline = try? JSONDecoder().decode([SambaOfflineMedia].self, from: data)
+        
+        guard let medias = mediasOffline else {
+            return nil
+        }
+        
+        return medias.map{$0.toSambaMedia()}
     }
     
 }
