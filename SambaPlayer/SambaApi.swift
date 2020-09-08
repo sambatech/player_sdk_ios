49c635571e17386ed793ade9444508a669ebf7ad
luis rodrigues <rodrigueslg@outlook.com>
2020-09-08 15:43:07 -0300

    removido comentário desnecessário

--- SambaPlayer/SambaApi.swift
+++ SambaPlayer/SambaApi.swift
@@ -176,8 +176,7 @@ fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
 		let playerConfig = json["playerConfig"]!! as AnyObject
 		let apiConfig = json["apiConfig"]!! as AnyObject
 		let project = json["project"]!! as AnyObject
-		
-        // aqui
+
         media.projectHash = project["playerHash"] as! String
 		media.projectId = project["id"] as! Int
         media.clientId = project["clientId"] as! Int
