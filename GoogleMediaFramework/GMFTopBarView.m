eaf8015208856216b9ecbceeebde8416124a90c1
Hayna Cardoso <hayna.cp@gmail.com>
2021-06-29 05:47:54 -0300

    Update GMFTopBarView.m

--- GoogleMediaFramework/GMFTopBarView.m
+++ GoogleMediaFramework/GMFTopBarView.m
@@ -103,11 +103,7 @@
                                                          metrics:metrics
                                                            views:viewsDictionary]];
     
-  constraints = [constraints arrayByAddingObjectsFromArray:
-                   [NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_videoTitle]|"
-                                                           options:NSLayoutFormatAlignAllCenterX
-                                                           metrics:metrics
-                                                             views:viewsDictionary]];
+  
   
   // Make the logo at most as wide the height of the bar.
   constraints = [constraints arrayByAddingObject:
